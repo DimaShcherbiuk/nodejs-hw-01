6dc88678d7d193f47a6257678fd837d823863159
DimaShcherbiuk <147422642+DimaShcherbiuk@users.noreply.github.com>
2024-07-05 19:14:01 +0300

    fix

--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -5,6 +5,10 @@ export const removeLastContact = async () => {
   try {
     const data = await fs.readFile(PATH_DB, 'utf8');
     const contacts = JSON.parse(data);
+    if (contacts.length === 0) {
+      console.log('No contacts to remove.');
+      return;
+    }
     contacts.pop();
     await fs.writeFile(PATH_DB, JSON.stringify(contacts, null, 2));
   } catch (error) {
